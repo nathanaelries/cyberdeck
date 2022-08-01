a242e3da13da9305b4dcf1673c9380fe5f2e84dc
nathanael.ries <1423199+nathanaelries@users.noreply.github.com>
2022-08-01 16:56:54 -0500

    Update cyberdeck.sh

--- cyberdeck.sh
+++ cyberdeck.sh
@@ -1,31 +1,11 @@
 #!/bin/bash
-
-#
-##
-## Downloading files...
-##
-#
-
-#Clean up previous installations
-rm ~/cyberdeck.log 2>/dev/null 
 rm -rf ~/cyberdeck
 mkdir -p ~/cyberdeck
-
-#We create all the needed folders for installation
 cd ~/cyberdeck
-
 #Cloning cyberdeck files
 git clone https://github.com/nathanaelries/cyberdeck.git ~/cyberdeck 
-
-#
-##
-## cyberdeck is installed, start setting up stuff
-##
-#
-
 for pkgName in $(cat ~/cyberdeck/packages.txt)
-
 do
-  pacman -S --force --noconfirm $pkgName
+pacman -S --force --noconfirm $pkgName
 done
 echo "Installed/Reinstalled all packages."
