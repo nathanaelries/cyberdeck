17ddaee2744e3d6e32aa9709636122276a36be1c
nathanael.ries <1423199+nathanaelries@users.noreply.github.com>
2022-08-01 16:31:21 -0500

    Update cyberdeck.sh

--- cyberdeck.sh
+++ cyberdeck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/nathanaelries/cyberdeck/main/packages.txt
-for pkgName in $(cat packages.txt)
+ 
+for pkgName in $(curl -O https://raw.githubusercontent.com/nathanaelries/cyberdeck/main/packages.txt)
 
 do
   pacman -S --force --noconfirm $pkgName
