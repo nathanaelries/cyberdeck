34d5d2ebcbb4114a0b50588e94c561fdbf80ef0a
nathanael.ries <1423199+nathanaelries@users.noreply.github.com>
2022-08-01 17:26:37 -0500

    Update cyberdeck.sh

--- cyberdeck.sh
+++ cyberdeck.sh
@@ -4,5 +4,4 @@ mkdir -p ~/cyberdeck
 cd ~/cyberdeck
 git clone https://github.com/nathanaelries/cyberdeck.git ~/cyberdeck
 for pkgName in $(cat ~/cyberdeck/packages.txt)
-do
-pacman -S --force --noconfirm $pkgName
+do pacman -S --force --noconfirm $pkgName done
